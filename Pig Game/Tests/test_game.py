f7a8fb3070ba898c6bfed79f2433f7a69b9bc1dc
Alice <vlasovaru931@gmail.com>
2022-11-25 01:34:56 +0300

    try

--- Pig Game/Tests/test_game.py
+++ Pig Game/Tests/test_game.py
@@ -60,6 +60,7 @@ def test_players_number_negativ():
     with MockInputFunction(side_effect=["a", "", "0", "2"]):
         assert PigGame._PigGame__num_players() == 2
 
+@patch('player.Player')
 def test_make_players():
     pl1 = Player("Alice", 4)
     pl2 = Player("Bob", 2)
@@ -155,13 +156,3 @@ def test_game_players_with_1():
             assert winner == "Alice"
             assert score == 10
 
-def test_game_init_intgr():
-    with MockInputFunction(side_effect=["20", "2", "Alice", "Bob"]):
-         with patch('random.randint', side_effect=[4, 2]):
-            game = PigGame()
-
-    assert game.win_score == 20
-    assert game.player_list[0].name == "Bob" and game.player_list[0].order == 2 and game.player_list[1].name == "Alice" and game.player_list[1].order == 4
-    assert hasattr(game, 'die')
-    assert game.roll_counter == 0
-    assert game.turn_score == 0
